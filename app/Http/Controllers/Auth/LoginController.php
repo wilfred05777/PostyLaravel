6971728f756d0c7320cabe7a47abbc73f4d7f91e
Wilfred <wilfred05777@gmail.com>
2021-05-21 17:04:40 +0800

    applying a middleware via route or in a controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+
     public function index(){
-
-
-
         return view('auth.login');
     }
 
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    // 2nd way of implementing a middleware
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
     public function index(){
         return view('dashboard');
     }
