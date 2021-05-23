d5cc12e6f6114d2fe58218ec770d5d1c163dd4c4
Wilfred <wilfred05777@gmail.com>
2021-05-24 04:52:19 +0800

    paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
     public function index(){
 
         // $posts = Post::get(); // laravel collection
-        $posts = Post::paginate(2);
+        $posts = Post::paginate(20);
 
         return view('posts.index', [
             'posts' => $posts
